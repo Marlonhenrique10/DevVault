ff034f0005a08c5b7baa417b7620280212ebabc9
Marlonhenrique10 <marlon_henrique2002@outlook.com>
2026-08-25 16:04:09 -0300

    feat(auth): add auth user repository

--- apps/auth-service/src/auth/auth.service.ts
+++ apps/auth-service/src/auth/auth.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { AuthUserRepository } from "./repositories/auth-user.repository";
 
 @Injectable()
 export class AuthService {
-    async signIn(email: string, password: string) {
-        return {
-            message: "User signed in successfully",
+    constructor(
+        private readonly authUserRepository: AuthUserRepository
+    ) {}
+
+    async findUserByEmail(email: string) {
+        const user = await this.authUserRepository.findByEmail(email);
+        if (!user) {
+            throw new NotFoundException('Usuário não encontrado para esse e-mail.');
         }
+        return user;
     }
+
+    async findUserById(id: string) {
+        const user = await this.authUserRepository.findById(id);
+        if (!user) {
+            throw new NotFoundException('Usuário não encontrado para esse ID.');
+        }
+        return user;
+    }
+
+    // async createUser(authUser: Partial<AuthUser>) {
+    //     const userExists = await this.authUserRepository.findByEmail(authUser.email);
+
+    //     if (userExists) {
+    //         throw new Error('Usuário com este e-mail já existe.')
+    //     }
+        
+    //     return this.authUserRepository.create(authUser);
+    // }
 }
